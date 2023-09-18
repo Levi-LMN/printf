d5caca6dcd294c4c55bc46c503c64ea393d01b32
Levi-LMN <Mukuhalevi@gmail.com>
2023-09-18 19:02:08 +0300

    Education is when you read the fine print. Experience is what you get if you don't

--- printf.c
+++ printf.c
@@ -1,71 +1,72 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
-void print_binary(unsigned int num)
-{
-    if (num > 1)
-        print_binary(num / 2);
-    putchar((num % 2) + '0');
-}
+#define BUFF_SIZE 1024
+
+typedef struct {
+    char flags;
+    int width;
+    int precision;
+    char size;
+} FormatSpecifier;
+
+void print_buffer(char buffer[], int *buff_ind);
+int parse_format(const char *format, va_list args);
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+    if (format == NULL)
+        return -1;
 
+    va_list args;
     va_start(args, format);
 
-    while (format && *format)
+    int printed_chars = parse_format(format, args);
+
+    va_end(args);
+
+    return printed_chars;
+}
+
+void print_buffer(char buffer[], int *buff_ind)
+{
+    if (*buff_ind > 0)
+        write(1, buffer, *buff_ind);
+    
+    *buff_ind = 0;
+}
+
+int parse_format(const char *format, va_list args)
+{
+    char buffer[BUFF_SIZE];
+    int buff_ind = 0;
+    int printed_chars = 0;
+
+    for (int i = 0; format[i] != '\0'; i++)
     {
-        if (*format == '%')
+        if (format[i] != '%')
         {
-            format++; /* Move past '%' */
-            if (!*format)
-                break;
-            else if (*format == 'c')
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
             {
-                char c = (char)va_arg(args, int);
-                putchar(c);
-                count++;
+                print_buffer(buffer, &buff_ind);
+                printed_chars += BUFF_SIZE;
             }
-            else if (*format == 's')
+            else
             {
-                const char *s = va_arg(args, const char*);
-                while (s && *s)
-                {
-                    putchar(*s);
-                    s++;
-                    count++;
-                }
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int num = va_arg(args, int);
-                printf("%d", num);
-                count++;
-            }
-            else if (*format == 'b')
-            {
-                unsigned int num = va_arg(args, unsigned int);
-                print_binary(num);
-                count++;
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                count++;
+                printed_chars++;
             }
         }
         else
         {
-            putchar(*format);
-            count++;
+            i++;
         }
-        format++;
     }
 
-    va_end(args);
-    return count;
+    print_buffer(buffer, &buff_ind);
+    printed_chars += buff_ind;
+
+    return printed_chars;
 }
 
