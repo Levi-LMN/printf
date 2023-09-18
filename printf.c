33e8d264b7c570a4d5d2a259318c705f82b2ad5f
Levi-LMN <Mukuhalevi@gmail.com>
2023-09-18 19:07:06 +0300

    Education is when you read the fine print. Experience is what you get if you don't

--- printf.c
+++ printf.c
@@ -1,54 +1,65 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
+
+/**
+ * _printf - Prints formatted output to stdout.
+ *
+ * @format: A format string.
+ * @...    : A variable number of arguments to be formatted.
+ *
+ * Returns: The number of characters printed (excluding the null byte used to end
+ * output to strings).
+ */
 int _printf(const char *format, ...)
 {
-    if (format == NULL)
-        return -1;
+  /**
+   * Create a variable argument list to iterate over the arguments.
+   */
+  va_list args;
+  va_start(args, format);
 
-    va_list args;
-    va_start(args, format);
+  /**
+   * Initialize the number of characters printed.
+   */
+  int count = 0;
 
-    int printed_chars = 0;
-
-    for (int i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++; // Move past '%'
-            if (format[i] == 'c')
-            {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                printed_chars++;
-            }
-            else if (format[i] == 's')
-            {
-                char *s = va_arg(args, char *);
-                if (s == NULL)
-                    s = "(null)";
-                for (int j = 0; s[j] != '\0'; j++)
-                {
-                    write(1, &s[j], 1);
-                    printed_chars++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                write(1, "%", 1);
-                printed_chars++;
-            }
-            // Add more conversion specifiers if needed
-        }
-        else
-        {
-            write(1, &format[i], 1);
-            printed_chars++;
-        }
+  /**
+   * Iterate over the format string and print each character, except for format
+   * specifiers. When a format specifier is encountered, print the corresponding
+   * argument value.
+   */
+  while (*format != '\0') {
+    if (*format == '%') {
+      switch (*(++format)) {
+        case 'c':
+          count += 1;
+          putchar(va_arg(args, int));
+          break;
+        case 's':
+          count += strlen(va_arg(args, char *));
+          puts(va_arg(args, char *));
+          break;
+        case '%':
+          count += 1;
+          putchar('%');
+          break;
+        default:
+          return -1;
+      }
+    } else {
+      count += 1;
+      putchar(*format);
     }
+    format++;
+  }
 
-    va_end(args);
+  /**
+   * Clean up the variable argument list.
+   */
+  va_end(args);
 
-    return printed_chars;
+  /**
+   * Return the number of characters printed.
+   */
+  return count;
 }
 
