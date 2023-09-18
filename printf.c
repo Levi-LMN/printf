c45e2bff5bf2ae77687cfad172079720859ce3eb
Levi-LMN <Mukuhalevi@gmail.com>
2023-09-18 19:41:52 +0300

    What one has not experienced, one will never understand in print

--- printf.c
+++ printf.c
@@ -1,6 +1,5 @@
-#include <stdarg.h>
-#include <stdio.h>
 #include "main.h"
+
 /**
  * _printf - A function that produces output according to a format
  * @format: A string that contains the text to be written to stdout
@@ -11,11 +10,6 @@ int _printf(const char *format, ...)
 {
     va_list args;
     int count = 0;
-    char* str;
-    char c;
-    unsigned int num;
-    char buffer[33];
-    char* p;
 
     va_start(args, format);
 
@@ -27,46 +21,15 @@ int _printf(const char *format, ...)
             switch (*format)
             {
                 case 'c':
-                    c = va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
                 case 's':
-                    str = va_arg(args, char*);
-                    while(*str)
-                    {
-                        putchar(*str);
-                        str++;
-                        count++;
-                    }
-                    break;
                 case '%':
-                    putchar('%');
-                    count++;
+                    count += handle_cs_percent(format, &args);
                     break;
                 case 'u':
-                    num = va_arg(args, unsigned int);
-                    sprintf(buffer, "%u", num);
-                    fputs(buffer, stdout);
-                    count += strlen(buffer);
-                    break;
                 case 'o':
-                    num = va_arg(args, unsigned int);
-                    sprintf(buffer, "%o", num);
-                    fputs(buffer, stdout);
-                    count += strlen(buffer);
-                    break;
                 case 'x':
-                    num = va_arg(args, unsigned int);
-                    sprintf(buffer, "%x", num);
-                    fputs(buffer, stdout);
-                    count += strlen(buffer);
-                    break;
                 case 'X':
-                    num = va_arg(args, unsigned int);
-                    sprintf(buffer, "%X", num);
-                    fputs(buffer, stdout);
-                    count += strlen(buffer);
+                    count += handle_uoxX(format, &args);
                     break;
                 default:
                     return (-1);
